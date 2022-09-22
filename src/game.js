34069ac197b186ad750740bdb0cc5c5f676aa72b
Angela Wirth <angiewirth92@gmail.com>
2022-09-22 11:51:47 -0600

    Add game properties again

---
+++ src/game.js
@@ -0,0 +1,18 @@
+class Game {
+  constructor() {
+    this.cat = new Player ('Cat', )
+    this.dog = new Player ('Dog')
+    this.classicIcons = [
+      './assets/happy-rocks.png',
+      '.assets/happy-paper.png',
+      '.assets/happy-scissors.png'
+    ]
+    this.difficultIcons = [
+      './assets/happy-rocks.png',
+      '.assets/happy-paper.png',
+      '.assets/happy-scissors.png',
+      '.assets/happy-alien.png',
+      '.assets/lizard.png'
+    ]
+  }
+}
