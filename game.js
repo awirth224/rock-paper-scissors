2511ac87c2c38637ff70b3f3c7c8dea627280898
Angela Wirth <angiewirth92@gmail.com>
2022-09-22 09:37:59 -0600

    Add properties and method

---
+++ game.js
@@ -0,0 +1,12 @@
+class Game {
+  constructor() {
+    this.cat = new Player ('Cat', 😸, choice)
+    this.dog = new Player ('Dog', 🐶, choice)
+    this.classicIcons = [/*imgs*/]
+    this.difficultIcons = [/*imgs*/]
+  }
+
+  checkForWin() {
+
+  }
+}
