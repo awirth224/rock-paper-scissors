cd08d10610df2babca068da6a703bb393dc544a7
Angela Wirth <angiewirth92@gmail.com>
2022-09-22 09:29:46 -0600

    Add properties and methods

---
+++ player.js
@@ -0,0 +1,21 @@
+class Player {
+  constructor() {
+    this.playerName = name
+    this.token = token
+    this.wins = 0
+    this.choice = icon || getRandomIcon()
+  }
+
+  geetRandomIcon() {
+
+  }
+
+  takeTurnClassic() {
+
+  }
+
+  takeTurnDifficult() {
+
+  }
+
+}
