26cbead7547c92465fe50187ad94a5d57036d736
Angela Wirth <angiewirth92@gmail.com>
2022-09-23 12:16:27 -0600

    Update player class to match game branch

--- src/game.js
+++ src/game.js
@@ -1,18 +1,27 @@
-class Game {
+class Game{
   constructor() {
-    this.cat = new Player ('Cat', token, icon)
-    this.dog = new Player ('Dog', token, icon)
-    this.classicIcons = [
-      './assets/happy-rocks.png',
-      '.assets/happy-paper.png',
-      '.assets/happy-scissors.png'
-    ]
-    this.difficultIcons = [
-      './assets/happy-rocks.png',
-      '.assets/happy-paper.png',
-      '.assets/happy-scissors.png',
-      '.assets/happy-alien.png',
-      '.assets/lizard.png'
-    ]
+    this.cat = new Player('Cat', token, choice)
+    this.dog = new Player('Dog', token, choice)
+    this.options = ['rock', 'paper', 'scissors', 'alien', 'lizard']
+    this.winOptions = {
+      rock: [scissors,lizard],
+      paper: [rock,alien],
+      scissors:[paper],
+      alien:[lizard,rock],
+      lizard:[paper, scissors]
+    } 
+  }
+
+      /*Lizard beats paper and scissors, loses to rock and alien
+      Alien beats lizard and rock, loses to paper and scissors */
+
+  takeTurnClassic(choice) {
+    this.cat.choiceClassic = choice 
+    this.dog.choiceClassic = this.dog.getRandomIcon(this.options)
+  }
+
+
+  checkWins() {
+
   }
 }
--- src/player.js
+++ src/player.js
@@ -1,27 +1,14 @@
 class Player {
-  constructor(name, token, icon) {
+  constructor(name, token, choice) {
     this.playerName = name
     this.token = token
     this.wins = 0
-    this.choiceClassic = icon || getRandomIcon(this.choicesClassic)
-    this.choiceDifficult = icon || getRandomIndex(this.choicesDifficult)
-    this.choicesClassic = ['rock', 'paper', 'scissors']
-    this.choicesDifficult = ['rock', 'paper', 'scissors', 'alien', 'lizard']
-    /*Lizard beats paper and scissors, loses to rock and alien
-      Alien beats lizard and rock, loses to paper and scissors */
+    this.choice = choice
   }
 
   getRandomIcon(array) {
     Math.floor(Math.random() * array.length)
   }
 
-  takeTurnClassic() {
-    
-
-  }
-
-  takeTurnDifficult() {
-
-  }
 
 }
