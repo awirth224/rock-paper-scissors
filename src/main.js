6c88b0d2ddae473622e46cf7e8fd78244ded3450
Angela Wirth <angiewirth92@gmail.com>
2022-09-24 11:14:12 -0600

    Add the word function to make functions work

--- src/main.js
+++ src/main.js
@@ -16,20 +16,20 @@ difficultGame.addEventListener('click', difficultGame)
 
 
 //functions
-classicGame(){
+function classicGame() {
   gameArea.classList.add('hidden')
   chooseFighter.classList.remove('hidden')
 }
 
 
-difficultGame() {
+function difficultGame() {
   classicGame()
   lizardAlien.classList.remove('hidden')
 
 }
 
 
-updateWinDisplay() {
+function updateWinDisplay() {
 
 }
 
