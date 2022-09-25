258bfc681b08cbdfb18f5bb5f05deb76e070a3ca
Angela Wirth <angiewirth92@gmail.com>
2022-09-25 17:21:36 -0600

    Add display functions and eventTargetIds to set playerChoice

--- src/main.js
+++ src/main.js
@@ -1,41 +1,81 @@
 //querySelectors
 var catWins = document.querySelector('.cat-wins')
 var dogWins = document.querySelector('.dog-wins')
+var catChoiceDisplay = document.querySelector('.cat-choice')
+var dogChoiceDisplay = document.querySelector('.dog-choice')
 var changingTitle = document.querySelector('.change-game-display')
 var clickGame = document.querySelector('.game-type')
 var chooseFighter = document.querySelector('.choose-fighter')
 var playersChoices = document.querySelector('.display-game')
 var classicGame = document.querySelector('.classic')
 var difficultGame = document.querySelector('.difficult')
-var lizardAlien = document.querySelector('.icon')
+var lizardAlien = document.querySelector('.difficult-icon')
 var classicIcons = document.querySelectorAll('.classic-icon')
 var classicButton = document.querySelector('.play-classic')
 var difficultButton = document.querySelector('.play-difficult')
 
+
 //eventListeners
 //one on game choice- classic or difficult
-classicButton.addEventListener('click', classicGame)
-difficultButton.addEventListener('click', difficultGame)
+classicButton.addEventListener('click', classicGameDisplay)
+difficultButton.addEventListener('click', difficultGameDisplay)
+
 
 //global variables
 currentGame = new Game()
 
 //functions
-function classicGame() {
+function classicGameDisplay() {
   gameArea.classList.add('hidden')
   chooseFighter.classList.remove('hidden')
   changingTitle.innerText = 'Choose Your Fighter'
 }
 
-
-
-function difficultGame() {
+function difficultGameDisplay() {
   classicGame()
   lizardAlien.classList.remove('hidden')
   changingTitle.innerText = 'Choose Your Fighter'
-
 }
 
+function classicGame() {
+ //is this needed --> classicGameDisplay()
+  currentGame.takeTurnClassic(event.target.id)
+}
+
+function difficultGame() {
+//is this needed -->  difficultGameDisplay()
+  currentGame.takeTurnDifficult(event.target.choice)
+}
+
+function displayCatChoice() {
+  if (event.target.id === 'rock') {
+    currentGame.cat.choice = 'rock'
+    catChoiceDisplay.innerHTML = `<img src="./assets/happy-rocks.png" alt="happy-rocks">`
+  }
+  else if(event.target.id === 'paper') {
+    currentGame.cat.choice = 'paper'
+    catChoiceDisplay.innerHTML = `<img src="./assets/happy-paper.png" alt="happy-paper">`
+  }
+  else if(event.target.id === 'scissors') {
+    currentGame.cat.choice = 'scissors'
+    catChoiceDisplay.innerHTML = `<img src="./assets/happy-scissors.png" alt="happy-scissors">`
+  }
+  else if(event.target.id === 'alien') {
+    currentGame.cat.choice = 'alien'
+    catChoiceDisplay.innerHTML = `<img src="./assets/happy-alien.png" alt="happy-alien">`
+  }
+  else if(event.target.id === 'lizard') {
+    currentGame.cat.choice = 'lizard'
+    catChoiceDisplay.innerHTML = `<img src="./assets/lizard.png" alt="lizard">`
+  }
+  
+}
+
+function displayDogChoice() {
+  if(currentGame.dog.choice === [0]) {
+    dogChoiceDisplay.innerHTML = ``
+  }
+}
 
 function updateWinDisplay() {
   catWins.innerHTML += `Wins: ${this.cat.wins}`
